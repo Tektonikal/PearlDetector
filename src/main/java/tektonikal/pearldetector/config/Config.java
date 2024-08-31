110d643f58216715118239469bbaed66506d94f8
Tektonikal <tek@tektonik.al>
2024-08-31 01:12:52 -0700

    rework almost done !

--- src/main/java/tektonikal/pearldetector/config/Config.java
+++ src/main/java/tektonikal/pearldetector/config/Config.java
@@ -4,22 +4,22 @@ import eu.midnightdust.lib.config.MidnightConfig;
 import net.minecraft.sound.SoundEvents;
 
 public class Config extends MidnightConfig {
-    @Entry(name = "Mod enabled")
+    @Entry(name = "Mod Enabled")
     public static boolean isEnabled = true;
-    @Entry(name = "Pearl scale", isSlider = true, min = 0.1F, max = 5F)
+    @Entry(name = "Pearl Scale", isSlider = true, min = 0.1F, max = 5F)
     public static float pearlScale = 1f;
-    @Entry(name = "Draw as lit")
-    public static boolean lit = true;
-    @Entry(name = "Play sound")
+    @Entry(name = "Light Level", isSlider = true, min = -1, max = 255)
+    public static int lightLevel = 255;
+    @Entry(name = "Play Sound")
     public static boolean playSound = true;
     @Entry(name = "Sound ID")
     public static String soundID = SoundEvents.ENTITY_ENDERMAN_TELEPORT.getId().toString();
-    @Entry(name = "Sound volume")
+    @Entry(name = "Sound Volume", isSlider = true, min = 0.1F, max = 3)
     public static float volume = 1;
-    @Entry(name = "Sound pitch")
+    @Entry(name = "Sound pitch", isSlider = true, min = 0.1F, max = 3)
     public static float pitch = 1;
-    @Entry(name = "Minimum detection distance")
+    @Entry(name = "Minimum Distance", min = 0, max = 10)
     public static float minDistance = 3.5F;
-    @Entry(name = "Maximum detection distance")
-    public static float maxDistance = 100000;
+    @Entry(name = "Maximum Distance", min = 20)
+    public static float maxDistance = 1000;
 }
--- src/main/java/tektonikal/pearldetector/mixin/FlyingItemEntityRendererMixin.java
+++ src/main/java/tektonikal/pearldetector/mixin/FlyingItemEntityRendererMixin.java
@@ -1,17 +1,22 @@
 package tektonikal.pearldetector.mixin;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.model.json.ModelTransformationMode;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.FlyingItemEntity;
 import net.minecraft.entity.projectile.thrown.EnderPearlEntity;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.InvalidIdentifierException;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
@@ -23,36 +28,64 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Mixin(net.minecraft.client.render.entity.FlyingItemEntityRenderer.class)
-public class FlyingItemEntityRendererMixin<T extends Entity> {
+public abstract class FlyingItemEntityRendererMixin<T extends Entity & FlyingItemEntity> extends EntityRenderer<T> {
     @Mutable
     @Final
-    @Shadow private float scale;
+    @Shadow
+    private float scale;
     @Mutable
-    @Shadow @Final private boolean lit;
+    @Shadow @Final private ItemRenderer itemRenderer;
     @Unique
     public List<Entity> notified = new ArrayList<>();
 
-    @Inject(method = "render", at = @At("TAIL"))
-    public void updated(T entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci){
-        ClientPlayerEntity player = MinecraftClient.getInstance().player;
-        if(Config.isEnabled && entity instanceof EnderPearlEntity){
-            this.scale = Config.pearlScale;
-            this.lit = Config.lit;
-        if(entity.age <= 2 && !notified.contains(entity) && entity.getPos().distanceTo(player.getEyePos()) > Config.minDistance && entity.getPos().distanceTo(player.getEyePos()) < Config.maxDistance){
-            if(Config.playSound){
-                try {
-                    Identifier soundId = new Identifier(Config.soundID);
-                    player.playSound(SoundEvent.of(soundId), Config.volume,Config.pitch);
-                } catch (InvalidIdentifierException exception) {
-                    player.playSound(SoundEvents.ENTITY_ENDERMAN_TELEPORT, 1,1);
-                }
-            }
-                notified.add(entity);
-        }
-        }
-        else{
-            this.scale = 1;
-        }
+    protected FlyingItemEntityRendererMixin(EntityRendererFactory.Context ctx) {
+        super(ctx);
     }
 
+    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
+    public void updated(T entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+        if (Config.isEnabled && entity instanceof EnderPearlEntity) {
+            this.scale = Config.pearlScale;
+            if ( entity.age < 1 &&
+                    !notified.contains(entity) &&
+                            entity.getPos().distanceTo(
+                                    new Vec3d(MinecraftClient.getInstance().player.getX(),
+                                            MinecraftClient.getInstance().player.getEyeY() - 0.10000000149011612,
+                                            MinecraftClient.getInstance().player.getZ())) > Config.minDistance &&
+                            entity.getPos().distanceTo(
+                                    new Vec3d(MinecraftClient.getInstance().player.getX(),
+                                            MinecraftClient.getInstance().player.getEyeY() - 0.10000000149011612,
+                                            MinecraftClient.getInstance().player.getZ())) < Config.maxDistance) {
+                if (Config.playSound) {
+                    try {
+                        Identifier soundId = Identifier.of(Config.soundID);
+                        MinecraftClient.getInstance().player.playSound(SoundEvent.of(soundId), Config.volume, Config.pitch);
+                    } catch (InvalidIdentifierException exception) {
+                        MinecraftClient.getInstance().player.playSound(SoundEvents.ENTITY_ENDERMAN_TELEPORT, 1, 1);
+                    }
+                }
+                notified.add(entity);
+            }
+
+            ci.cancel();
+        } else {
+            scale = 1;
+        }
+        matrices.push();
+        matrices.scale(this.scale, this.scale, this.scale);
+        matrices.multiply(this.dispatcher.getRotation());
+        this.itemRenderer.renderItem(((FlyingItemEntity) entity).getStack(), ModelTransformationMode.GROUND, getLight(entity), OverlayTexture.DEFAULT_UV, matrices, vertexConsumers, entity.getWorld(), entity.getId());
+        matrices.pop();
+        super.render(entity, yaw, tickDelta, matrices, vertexConsumers, getLight(entity));
+    }
+    @Unique
+    private int getLight(T entity){
+        if(Config.isEnabled && Config.lightLevel != -1){
+            return Config.lightLevel;
+        }
+        else{
+            BlockPos blockPos = BlockPos.ofFloored(entity.getX(), entity.getY(), entity.getZ());
+            return entity.getWorld().isChunkLoaded(blockPos) ? WorldRenderer.getLightmapCoordinates(entity.getWorld(), blockPos) : 0;
+        }
+    }
 }
